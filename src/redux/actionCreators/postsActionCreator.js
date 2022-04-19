85dc52603e04a4adfc2a4a30c9cf105b35323c32
ValeryGil <valery.gil@icloud.com>
2022-04-19 12:40:45 +0300

    Добавляет модуль удаление поста

--- src/redux/actionCreators/postsActionCreator.js
+++ src/redux/actionCreators/postsActionCreator.js
@@ -1,5 +1,5 @@
 import { API_TOKEN } from "../../tokens"
-import { SET_ALL_POSTS, ADD_NEW_POST } from "../types/postsTypes"
+import { SET_ALL_POSTS, ADD_NEW_POST, DELETE_POST } from "../types/postsTypes"
 
 export const setAllPosts = (allPosts) => ({
   type: SET_ALL_POSTS,
@@ -36,4 +36,21 @@ export const loadNewPost = (post) => async (dispatch) => {
   })
   const postsFromApi = await response.json()
   dispatch(addNewPost(postsFromApi))
+}
+
+export const deletePost = (_id) => ({
+  type: DELETE_POST,
+  payload: _id,
+})
+
+export const deletePostQuery = (_id) => async (dispatch) => {
+  const response = await fetch(`https://api.react-learning.ru/posts/${_id}`, {
+    method: 'DELETE',
+    headers: {
+      authorization: `Bearer ${API_TOKEN}`,
+      'Content-Type': 'application/json',
+  }})
+  if (response.status === 200) {
+    dispatch(deletePost(_id))
+  }
 }
